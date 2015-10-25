f98b799884426323d343a967d0d71768e2df8896
kevintrinh <kevintrinh255@gmail.com>
2015-10-24 22:35:23 -0700

    ...

--- Hash.java
+++ Hash.java
@@ -1,35 +1,35 @@
 import java.util.*;
 
-public class Hash 
-{
-	public static void main(String[] args)
-	{
+public class Hash {
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Map<Integer, TreeSet<String>> myMap = new HashMap<Integer, TreeSet<String>>();
-		while(in.hasNext()){
+		while (in.hasNext()) {
 			String process = in.next();
 			int hashKey = process.hashCode();
 			TreeSet<String> currentSet = myMap.get(hashKey);
-			if(currentSet != null){
+			if (currentSet != null) {
 				currentSet.add(process);
-			}else{
+			} else {
 				currentSet = new TreeSet<String>();
 				currentSet.add(process);
 			}
 			myMap.put(hashKey, currentSet);
 		}
-		for(int currentKey : myMap.keySet()){
+		for (int currentKey : myMap.keySet()) {
 			TreeSet<String> currentSet = myMap.get(currentKey);
 			System.out.print(currentKey + ": ");
 			Iterator<String> hashIter = currentSet.iterator();
-			while(hashIter.hasNext()){
-				if(hashIter.hasNext()){
-					System.out.print(hashIter.next() + ", ");
-				}else{
-					System.out.print(hashIter.next());
+			if (currentSet.size() > 1) {
+				while (hashIter.hasNext()) {
+					if (hashIter.hasNext()) {
+						System.out.print(hashIter.next() + ", ");
+					} else {
+						System.out.print(hashIter.next());
+					}
 				}
+				System.out.println();
 			}
-			System.out.println();
 		}
 		in.close();
 	}
